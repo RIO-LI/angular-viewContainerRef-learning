8d798eb26ed9867e63687fac565104fbc7458b56
1406081008@qq.com <cjt19930809>
2018-12-25 18:30:57 +0800

    新增ngtemplate-insert-into-ngcontainer-viewcontainer项目，用来演示以ng-container为视图容器，动态插入ng-template模版内容的例子

---
+++ ngtemplate-insert-into-ngcontainer-viewcontainer/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, AfterViewInit, ViewChild, ViewContainerRef, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container', { read: ViewContainerRef })
+  private vcr: ViewContainerRef;
+
+  @ViewChild(TemplateRef)
+  private tpl: TemplateRef<HTMLElement>;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.vcr.createEmbeddedView(this.tpl);
+  }
+}
